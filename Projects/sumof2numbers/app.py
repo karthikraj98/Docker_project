f53ccab1e22b43b41ba917ea3321f1f08f15cb5e
Bayanna Karthik <126453814+karthikraj98@users.noreply.github.com>
2026-01-22 14:59:48 +0530

    Add files via upload

---
+++ Projects/sumof2numbers/app.py
@@ -0,0 +1,9 @@
+print('sum of two numbers')
+
+number1 = int(input('enter first number'))
+
+number2 = int(input('enter second number'))
+
+result = number1 + number2
+
+print(f'sum of 2 numbers{result}')
